f28628d300324c9c5924762207288c5bf577024a
Vincenzo Iuliano <iuliano-vincenzo@live.it>
2025-01-08 16:21:19 +0100

    ADD db js file

---
+++ data/db.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'minuscolo',
+    database: 'films_db'
+})
+
+connection.connect(() => {
+    if(err) throw err
+
+    console.log('Connect to MYSQL');
+})
+
+module.exports = connection
